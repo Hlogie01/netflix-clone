d4c8db7a0550011f237b68f3dc708ce8cddbdfcb
Hlogie01 <135062998+Hlogie01@users.noreply.github.com>
2024-07-11 13:01:07 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,11 @@ function Navbar() {
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="Netflix Logo"
       />
+      <img
+        className="navbar__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Avatar"
+      />
     </div>
   );
 }
